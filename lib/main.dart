48e012d95554eec040edb3b1969fb5924aee9466
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-07-23 00:12:05 +0530

    added color and theme check

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 58, 169, 183)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Home Page'),
@@ -39,13 +40,124 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text(
-          'Welcome!',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.inversePrimary,
-            fontSize: 60,
-            letterSpacing: 8,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Welcome!',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontSize: 60,
+                letterSpacing: 8,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.background,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('background'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.error,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('error'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+              height: 40,
+              width: 600,
+              child: Text(
+                'onBackground',
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.onError,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('onError'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('onPrimary'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('onSecondary'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+              height: 40,
+              width: 600,
+              child: Text(
+                'onSurface',
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('primary'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('secondary'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent),
+                color: Theme.of(context).colorScheme.surface,
+              ),
+              height: 40,
+              width: 600,
+              child: const Text('surface'),
+            ),
+          ],
         ),
       ),
     );
